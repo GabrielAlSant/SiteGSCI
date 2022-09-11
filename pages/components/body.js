3e77a37d5339522b5daa988831c292f087834436
Gabriel Alves dos Santos <gabrielalvesdossantos16@gmail.com>
2022-09-11 14:31:43 -0300

    testheader

--- pages/components/body.js
+++ pages/components/body.js
@@ -4,8 +4,9 @@ import img from '../../public/img/utilizavel.jpg'
 export default function Body(){
     return (
         <div className="container">
-            <div>
+            <div className="bodyimg">
              <Image src={img} className="img1"></Image>
+             <div className="imgtext"></div>
             </div>
         </div>
     )
