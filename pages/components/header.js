705964287503e21dac4e1064c7bf146d85096e96
Gabriel Alves dos Santos <gabrielalvesdossantos16@gmail.com>
2022-09-11 12:24:11 -0300

    class

--- pages/components/header.js
+++ pages/components/header.js
@@ -5,24 +5,24 @@ export default function Header(){
         <div className="row">
         <div className="col-sm-8"><span className="title">Book&Book</span></div>
         <div className="col-sm-4"> 
-        <nav class="navbar navbar-expand-lg bg">
+        <nav className="navbar navbar-expand-lg bg">
          <div>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link" href="#"><span className="headitem">Login</span></a>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link" href="#"><span className="headitem">Login</span></a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#"><span className="headitem">Produtos</span></a>
+        <li className="nav-item">
+          <a className="nav-link" href="#"><span className="headitem">Produtos</span></a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#"><span className="headitem">Sacola</span></a>
+        <li className="nav-item">
+          <a className="nav-link" href="#"><span className="headitem">Sacola</span></a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#"><span className="headitem">Usuário</span></a>
+        <li className="nav-item">
+          <a className="nav-link" href="#"><span className="headitem">Usuário</span></a>
         </li>
       </ul>
     </div>
