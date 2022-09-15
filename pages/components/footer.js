321ccbe8864ffd65c0182717318cdc70ae0a4975
Gabriel Alves dos Santos <gabrielalvesdossantos16@gmail.com>
2022-09-15 06:23:24 -0300

    footer

--- pages/components/footer.js
+++ pages/components/footer.js
@@ -1,16 +1,16 @@
 export default function Footer(){
 return (
     <div>
-    <div className="displayy">
+    <div className="displayf">
       
       <div className="start"><span className="title">Livraria</span></div>
-      <div className="end"> </div>
+      <div className="endf"> </div>
     
     </div>
-    <div className="displayy">
+    <div className="displayf">
       
-      <div className="start"><span className="padd">Sobre</span><span className="padd">Contato</span></div>
-      <div className="end"><span className="margfooter">Todos os direitos reservados</span></div>
+      <div className="startf"><span className="padd">Sobre</span><span className="padd">Contato</span></div>
+      <div className="endf"><span className="margfooter">@Todos os direitos reservados</span></div>
     
     </div>
     </div>
